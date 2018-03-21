e6109ed61635edc80d60e2f364cf174cc06abfc6
Mohmmad Mansuri <mohmmadmansuri7767@gmail.com>
2018-03-21 23:16:02 +0530

    make it better

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', debug=True, port=port)
+    app.run(host='https://skindiseasepredictionbymohmmad.herokuapp.com/', debug=True, port=port)
