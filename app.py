b6f1b6fd2a1a2025c7cdd239bce18d9aed0033e7
Mohmmad Mansuri <mohmmadmansuri7767@gmail.com>
2018-03-22 00:04:29 +0530

    make it better

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def homepage():
 
 
 if __name__ == '__main__':
-    #port = int(os.environ.get('PORT', 33507))
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(debug=True, port=port)
     #debug=True , port=port host='https://skindiseasepredictionbymohmmad.herokuapp.com/', 
