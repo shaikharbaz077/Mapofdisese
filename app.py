3cb1e603277741b8f2b2227c338c0e6208be75f0
Mohmmad Mansuri <mohmmadmansuri7767@gmail.com>
2018-03-21 23:54:58 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return "<h1>hello world</h1>"
+
+
+if __name__ == '__main__':
+    #port = int(os.environ.get('PORT', 33507))
+    app.run(debug=True)
+    #debug=True , port=port host='https://skindiseasepredictionbymohmmad.herokuapp.com/', 
--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import app
-
-#app = Flask(__name__)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 33507))
-    app.run(host='https://skindiseasepredictionbymohmmad.herokuapp.com/', debug=True , port=port)
