dba43e52209f0f84611e6b4799f74d76e865b7b9
Mohmmad Mansuri <mohmmadmansuri7767@gmail.com>
2018-03-22 16:10:02 +0530

    make it better 2

---
+++ settings.py
@@ -0,0 +1,9 @@
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
